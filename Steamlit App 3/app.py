c105d1b89edadf0b6cd2d71ee3c8498cb1bcec2d
arhamansari11 <arhamansari454647@gmail.com>
2024-08-01 09:50:10 +0500

    Streamlit app 03

--- Steamlit App 3/app.py
+++ Steamlit App 3/app.py
@@ -1,21 +1,26 @@
-# import streamlit as st
-# st.title("First StreamLit App")
-# input_text = st.text_area("Enter your text : ")
-# if st.button("Print"):
-#     st.write(input_text)
 import streamlit as st
 
-code = '''def hello():
-    print("Hello, Streamlit!")'''
-st.code(code)
+# code = '''def hello():
+#     print("Hello, Streamlit!")'''
+# st.code(code)
 
 
-try_code = ''' def function():
-    print("I am Muhammad Arham") '''
+# try_code = ''' def function():
+#     print("I am Muhammad Arham") '''
 
-variable  = "I am Muhammad Arham and I am a MERN Stack Develoepr"
-st.code(variable)
-st.code(try_code)
-st.header("Hello World !")
-st.title("Hello World !")
-st.caption("Hello World !")
+# variable  = "I am Muhammad Arham and I am a MERN Stack Develoepr"
+# st.code(variable)
+# st.code(try_code)
+# st.header("Hello World !")
+# st.title("Hello World !")
+# st.caption("Hello World !")
+
+st.write("This is some text")
+
+st.slider("This is Slider" , 0 , 100 , (25,75))
+
+st.divider()
+st.write("This is Divider")
+st.divider()
+
+
---
+++ Steamlit App 3/input.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# options = st.multiselect(
+#     "What are your favorite colors",
+#     ["Green", "Yellow", "Red", "Blue"],
+#     ["Yellow", "Red"],
+# )
+
+# st.write("You selected:", options)
+
+# st.link_button("Go to my PortFolio" , "https://muhammad-arham.netlify.app/")
+
+# color = st.color_picker("Pick a Color" , "#00f900")
+# st.write("You Pick" , color )
+
+
+# option = st.selectbox(
+#     "How would you like to be contacted?",
+#     ("Email", "Home phone", "Mobile phone"),
+#     index=None,
+#     placeholder="Select contact method...",
+# )
+
+# st.write("You selected:", option)
+
+# with st.sidebar:
+#     messages = st.container(height=300)
+#     if prompt := st.chat_input("Say something"):
+#         messages.chat_message("user").write(prompt)
+#         messages.chat_message("assistant").write(f"Echo: {prompt}")
+
